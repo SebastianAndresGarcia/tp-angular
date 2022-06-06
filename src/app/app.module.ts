b726ac2ce9fe36f87f2b7ad8d123fa66ec9960bc
SebastianAndresGarcia <sebastiana.garcia@alumnos.frm.utn.edu.ar>
2022-06-06 10:42:44 -0300

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { TarjetainstrumentoComponent } from './components/tarjetainstrumento/tarjetainstrumento.component';
+import { DetalleinstrumentoComponent } from './components/detalleinstrumento/detalleinstrumento.component';
+import { BuscadorComponent } from './components/buscador/buscador.component';
+import { DondeestamosComponent } from './components/dondeestamos/dondeestamos.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    AboutComponent,
+    TarjetainstrumentoComponent,
+    DetalleinstrumentoComponent,
+    BuscadorComponent,
+    DondeestamosComponent,
+  
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    
+  ],
+  
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/components/tarjetainstrumento/tarjetainstrumento.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { ServinstrumentoService } from 'src/app/servicio/servinstrumento.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'tarjetainstrumento',
+  templateUrl: './tarjetainstrumento.component.html',
+  styleUrls: ['./tarjetainstrumento.component.css']
+})
+export class TarjetainstrumentoComponent implements OnInit {
+
+  instrumentosArr:any[] = [];
+  
+  //al instanciar el componente se llama al servicio
+  constructor(private serviinstrumento: ServinstrumentoService, private router:Router) { }
+
+  ngOnInit(): void {
+    this.instrumentosArr = this.serviinstrumento.getInstrumentos(); 
+    console.log(this.instrumentosArr);
+  }
+  public verInstrumento(idx:string){
+    this.router.navigate(['/detalleinstrumento', idx]) 
+  }
+}
